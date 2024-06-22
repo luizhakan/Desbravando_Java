2fda346232589680a90785b4c57a4da39d5a61ac
luizhakan <luizhakan2@protonmail.com>
2024-06-22 17:37:43 -0300

    Capitulo 1 - Java

---
+++ src/Argumentos.java
@@ -0,0 +1,5 @@
+public class Argumentos {
+    public static void main(String[] args) {
+        System.out.printf("O primeiro de muitos códigos escritos em %s por %s\n", args[0], args[1]);
+    }
+}
